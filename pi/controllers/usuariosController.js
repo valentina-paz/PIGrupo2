d57959705926cc3db6e3f74e3883da8353261ac2
valentina-paz <vpaz@udesa.edu.ar>
2023-11-01 16:27:49 -0300

    seguimos con mi perfil -- terminar

--- pi/controllers/usuariosController.js
+++ pi/controllers/usuariosController.js
@@ -1,23 +1,24 @@
 const data= require("../database/models");
 let usuarios= data.Usuario;
-let posteos= data.Posteos;
-
 
 const usuariosController = {
     miPerfil: function(req, res, next) {
-        let criterio = {where: [{
-            id: req.session.user.id
-        }]};
-        res.send(req.session.user.id)
-
-        posteos.findAll(criterio)
-        .then(function (result) {
-            return res.render ('miPerfil',{posteos:result})
+        let id= req.params.id;
+        // return res.send(req.params.id)
+        let relacion = {
+            include: {
+                all: true,
+                nested: true
+            }
+        };
+        usuarios.findByPk(id, relacion)
+        .then(function(result){
+            res.send(result)
+            //return res.render('detalleUsuario', { idUsuario: id, usuario: result, posteos: result.usuarioPosteo});
         })
         .catch(function (error) {
             return res.send(error)
         })
-        // res.send({data:res.session.user})
     },
     editarPerfil: function(req, res, next) {
         return res.render('editarPerfil', { title: 'Express' });
@@ -32,8 +33,8 @@ const usuariosController = {
         };
         usuarios.findByPk(id, relacion)
         .then(function(result){
-            // res.send(result)
-            return res.render('detalleUsuario', { idUsuario: id, usuario: result, posteos: result.usuarioPosteo });
+            res.send(result)
+            //return res.render('detalleUsuario', { idUsuario: id, usuario: result, posteos: result.usuarioPosteo });
         })
         .catch(function(error){
             return res.send(error)
